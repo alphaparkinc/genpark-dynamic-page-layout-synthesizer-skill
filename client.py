44306857fca995d49c1733068f0a573df2bda067
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-11 07:33:12 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,12 @@
+class DynamicSparkpageLayoutSynthesizerClient:
+    def synthesize_sparkpage_layout(self, search_topic='Top 10 Humanoid AI Robots 2026', entity_cards_count=8):
+        return {
+            'layout_id': 'spk_syn_6620',
+            'search_topic': search_topic,
+            'rendered_entity_cards': entity_cards_count,
+            'comparison_matrix_columns': 6,
+            'glassmorphism_theme': 'DARK_GRADIENT_OUTFIT',
+            'interactive_copilot_dock_enabled': True,
+            'static_bundle_size_kb': 28.4,
+            'sparkpage_live_preview_url': 'https://sparkpages.genpark.ai/preview/6620.html'
+        }
