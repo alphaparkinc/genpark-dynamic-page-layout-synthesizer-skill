1aaa4d2d7b9781129a8086c26d7cc65a06ce0647
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-21 18:17:24 +0800

    refactor: replace sparkpage with page in client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-class DynamicSparkpageLayoutSynthesizerClient:
-    def synthesize_sparkpage_layout(self, search_topic='Top 10 Humanoid AI Robots 2026', entity_cards_count=8):
+class DynamicPageLayoutSynthesizerClient:
+    def synthesize_page_layout(self, search_topic='Top 10 Humanoid AI Robots 2026', entity_cards_count=8):
         return {
             'layout_id': 'spk_syn_6620',
             'search_topic': search_topic,
@@ -8,5 +8,5 @@ class DynamicSparkpageLayoutSynthesizerClient:
             'glassmorphism_theme': 'DARK_GRADIENT_OUTFIT',
             'interactive_copilot_dock_enabled': True,
             'static_bundle_size_kb': 28.4,
-            'sparkpage_live_preview_url': 'https://sparkpages.genpark.ai/preview/6620.html'
+            'page_live_preview_url': 'https://pages.genpark.ai/preview/6620.html'
         }
