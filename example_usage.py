dbe50cdb123918528bac26025638715b996773ce
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-21 18:17:27 +0800

    refactor: replace sparkpage with page in example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -1,11 +1,11 @@
-from client import DynamicSparkpageLayoutSynthesizerClient
+from client import DynamicPageLayoutSynthesizerClient
 
 def main():
-    client = DynamicSparkpageLayoutSynthesizerClient()
-    res = client.synthesize_sparkpage_layout('Top 10 Humanoid AI Robots 2026', 8)
-    print('Sparkpage Synthesizer: ' + res['layout_id'] + ' (' + res['search_topic'] + ')')
+    client = DynamicPageLayoutSynthesizerClient()
+    res = client.synthesize_page_layout('Top 10 Humanoid AI Robots 2026', 8)
+    print('Page Synthesizer: ' + res['layout_id'] + ' (' + res['search_topic'] + ')')
     print('Cards: ' + str(res['rendered_entity_cards']) + ' | Columns: ' + str(res['comparison_matrix_columns']))
-    print('Live Preview: ' + res['sparkpage_live_preview_url'])
+    print('Live Preview: ' + res['page_live_preview_url'])
 
 if __name__ == '__main__':
     main()
